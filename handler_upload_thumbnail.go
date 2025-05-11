eb34dad052ec8f4fa58772fa35a38d972492121f
kappapee <kappapee@pm.me>
2025-05-11 11:13:24 +0300

    storage and cache stuff

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -40,10 +46,31 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	}
 
 	contentType := header.Header.Get("Content-Type")
-	mediaData, err := io.ReadAll(file)
+	mediaType, _, err := mime.ParseMediaType(contentType)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Unable to read data file", err)
+		respondWithError(w, http.StatusInternalServerError, "Unable to process mime-type", err)
 	}
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusUnsupportedMediaType, "Unsupported MIME type", nil)
+	}
+
+	contentTypeParts := strings.Split(mediaType, "/")
+	fileExtension := contentTypeParts[len(contentTypeParts)-1]
+
+	randomName := make([]byte, 32)
+	rand.Read(randomName)
+	randomNameURL := make([]byte, base64.RawURLEncoding.EncodedLen(len(randomName)))
+	base64.RawURLEncoding.Encode(randomNameURL, randomName)
+	randomNameString := string(randomNameURL)
+
+	filename := fmt.Sprintf("%s.%s", randomNameString, fileExtension)
+	filePath := filepath.Join(cfg.assetsRoot, filename)
+
+	mediaFile, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to create file", err)
+	}
+
 	metaData, err := cfg.db.GetVideo(videoID)
 	if err != nil {
 		respondWithError(w, http.StatusNotFound, "Unable to retrieve video from DB", err)
@@ -53,13 +80,12 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		respondWithError(w, http.StatusUnauthorized, "Not authorized", err)
 	}
 
-	thumb := thumbnail{
-		data:      mediaData,
-		mediaType: contentType,
+	_, err = io.Copy(mediaFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to write content to file", err)
 	}
 
-	videoThumbnails[videoID] = thumb
-	thumbURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	thumbURL := fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, filename)
 	metaData.ThumbnailURL = &thumbURL
 
 	err = cfg.db.UpdateVideo(metaData)
